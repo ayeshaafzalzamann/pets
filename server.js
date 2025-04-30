dec0e3ce922f9309dd5cbcb9a5b192ed8c2f0ec0
ayeshaafzalzaman <154813489+ayeshaafzalzamann@users.noreply.github.com>
2025-04-30 10:31:30 +0400

    Update MongoDB connection string

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/petsDB', {
+mongoose.connect('mongodb+srv://ayesha33afzal:lbE1CpIJlhFR6AnV@cluster0.4dmiety.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
